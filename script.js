83c6511b18634b80e025d641b408e60dae38edf4
Gustavo-GS <gustavogs@gustavo.bbrouter>
2022-03-09 20:32:33 -0300

    Adicionado Object.keys + comentário explicativo

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ myObject['lastName'] = 'Soares Thomann';
 myObject.favoriteColor = 'Blue';
 
 // console.table(myObject);
+
+
+
+const objectKeys = {
+    name: 'Sebastian',
+    lastName: 'Soares',
+    favoriteSerie: 'Supernatural',
+    cpf: '123.456.789-10',
+}
+
+console.log(Object.keys(objectKeys));
+
+/* O comando "Object.keys" como o próprio nome já diz,
+ele pega as "chaves" de um objeto. */
+
+//  Sintaxe:  -  Object.keys(nome do objeto);
