75c9a690fd74a1c7f1e38dd212e1f64d1370dfe7
Gustavo-GS <gustavogs@gustavo.bbrouter>
2022-03-09 19:46:59 -0300

    Adicionado uma função que diz quantos dias tem o mês

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ const sum = (n1, n2) => {
 /*  E se por acausa algo der errado por exemplo:
 um valor ser do tipo 'number' e o outro ser do tipo 'string', 
 o comando (throw new Error) vai ser chamado. */
+
+
+
+//  função que diz quantos dias tem o mês que escolher
+const quantosDiasTemOMes = (inputNumber) => {
+    switch(parseInt(inputNumber)){
+        case 1:
+            return 31;
+        case 2:
+            return 28;
+        case 3:
+            return 31;
+        case 4:
+            return 30;
+        case 5:
+            return 31;
+        case 6:
+            return 30;
+        case 7:
+            return 31;
+        case 8:
+            return 31;
+        case 9:
+            return 30;
+        case 10:
+            return 31;
+        case 11:
+            return 30;
+        case 12:
+            return 31;
+        default:
+            throw new Error('Mês Invalido!');
+    };
+};
