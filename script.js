a5fed42227ca416eac23aa455db8bdb0785ddfb4
Gustavo-GS <gustavogs@gustavo.bbrouter>
2022-03-09 20:36:35 -0300

    Adicionado Object.values + comentário explicativo

--- script.js
+++ script.js
@@ -25,3 +25,19 @@ console.log(Object.keys(objectKeys));
 ele pega as "chaves" de um objeto. */
 
 //  Sintaxe:  -  Object.keys(nome do objeto);
+
+
+
+const objectValues = {
+    name: 'Matheus',
+    lasName: 'Vieira',
+    favoriteColor: 'Green',
+    cpf: '098.765.432-10',
+}
+
+console.log(Object.values(objectValues));
+
+/* O comando "Object.values" é a quase a mesma coisa que o "Object.keys",
+só que ao invés de pegar as chaves, ele pega os "valores" de um objeto. */
+
+//  Sintaxe:  -  Object.values(nome do objeto);
