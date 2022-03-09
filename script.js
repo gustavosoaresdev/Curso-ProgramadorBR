7a729b77ac2f0b73910cbca0ca18f6b677ec59a7
Gustavo-GS <gustavogs@gustavo.bbrouter>
2022-03-09 20:25:50 -0300

    Só um teste básico

---
+++ script.js
@@ -0,0 +1,11 @@
+//  Só um teste básico
+
+const myObject = {
+    name: 'Gustavo',
+    age: 19,
+}
+
+myObject['lastName'] = 'Soares Thomann';
+myObject.favoriteColor = 'Blue';
+
+// console.table(myObject);
