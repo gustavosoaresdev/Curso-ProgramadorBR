9e023cc22b3904f692318327ac9812b37d4a7f75
Gustavo-GS <gustavogs@gustavo.bbrouter>
2022-03-11 20:05:50 -0300

    Adicionado find

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-const nomes = [
-    'Gustavo',
-    'Sebastian',
-    'Christofer',
-    'Amanda',
-    'Fernanda',
+//  PARA UM ARRAY DE NOMES
+const fruits = [
+    'Maça',
+    'Banana',
+    'Abacaxi',
+    'Melancia',
+    'Laranja',
 ];
 
-const nome = nomes.forEach((element, index, array) => {
-    console.log(index, element, array);
-});
+const foundFruits = fruits.find(p => p.startsWith('M'));
+console.log(foundFruits);
 
-/* O método forEach() executa uma dada função
-em cada elemento de um array. */
+
+
+//  PARA UM ARRAY DE OBJETOS
+const peoples = [
+    { name: 'Gustavo', lastName: 'Soares' },
+    { name: 'Sebastian', lastName: 'Soares' },
+    { name: 'Christofer', lasName: 'Oliveira' },
+    { name: 'Amanda', lastName: 'Silva' },
+    { name: 'Fernanda', lastName: 'Rodriguês' },
+];
+
+const foundPeoples = peoples.find( n => n.name.startsWith('S'));
+console.log(foundPeoples);
+
+
+
+/* O método find() retorna o valor do primeiro elemento do array
+que satisfizer a função de teste provida.
+Caso contrario, undefined é retornado. */
+
+/* Se por exemplo você pesquisar algo por uma letra
+em um array, e tiver duas palavras com a mesma letra inicial,
+ela vai aparecer apenas a primeira palavra com a letra igual. */
