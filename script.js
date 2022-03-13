53f2840d69d1eae4359c0bbb0514c8ed2db8942b
Gustavo-GS <gustavogs@gustavo.bbrouter>
2022-03-12 22:19:57 -0300

    Adicionado botão dentro do body

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ function createButton (text, callback) {
     const body = document.querySelector('body');
     const button = document.createElement('button');
     button.textContent = text;
-}
+
+    callback(button);
+
+    body.appendChild(button);
+};
