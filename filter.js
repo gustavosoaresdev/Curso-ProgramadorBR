96579297d51ad5516a7b91213c57e1f0c8a822fc
Gustavo-GS <gustavogs@gustavo.bbrouter>
2022-03-14 06:53:19 -0300

    Adicionado comentário explicativo / portuguese & english

--- filter.js
+++ filter.js
@@ -10,3 +10,13 @@ const newUser = [
 
 const over18 = newUser.filter( age18 => age18.age >= 18);
 console.log(over18);
+
+
+
+//  Portuguese
+/* O método filter() cria um novo array com todos os elementos
+que passaram no teste implementado pela função fornecida. */
+
+//  English
+/* The filter() creates a new array with all elements
+that has not been tested by the support function. */
